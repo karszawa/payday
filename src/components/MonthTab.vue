255f8cb7f4b767ac0abc0530ba3caa945e45f6f1
Hiroaki KARASAWA <siqvare@gmail.com>
2018-09-01 21:24:07 +0900

    Implement header

---
+++ src/components/MonthTab.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+  .container()
+    .target-month {{ formattedTargetMonth }}
+    .under-triangle
+</template>
+
+<script lang="ts">
+import { mapState, mapMutations } from 'vuex';
+
+export default {
+  computed: {
+    formattedTargetMonth(): string {
+      return this.$store.state.targetMonth.toFormat('LLLL yyyy');
+    },
+  },
+  method: {
+    ...mapMutations([ 'openMonthSelectionDialog' ]),
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.container {
+  padding-top: 20px;
+  padding-bottom: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #303f9f;
+  color: white;
+}
+
+.under-triangle {
+  margin-left: 8px;
+  margin-top: 3px;
+  width: 0px;
+  height: 0px;
+  border: 6px solid transparent;
+  border-top: 6px solid white;
+}
+</style>
--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,29 @@
 import Vue from 'vue';
-import Vuex from 'vuex';
+import Vuex, { StoreOptions } from 'vuex';
+import { DateTime } from 'luxon';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    monthSelectionDialogOpended: false,
+    targetMonth: DateTime.local(),
   },
   mutations: {
-
+    openMonthSelectionDialog(state) {
+      state.monthSelectionDialogOpended = true;
+    },
+    closeMonthSelectionDialog(state) {
+      state.monthSelectionDialogOpended = false;
+    },
+    setTargetMonth(state, dateTime: DateTime) {
+      state.targetMonth = dateTime;
+    },
   },
   actions: {
+
+  },
+  getters: {
 
   },
 });
